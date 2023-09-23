8f606b3255c493ba248a115cc35d272b45fe40cb
Pedro <72952088+Batacoito@users.noreply.github.com>
2023-09-22 21:52:42 -0300

    Update TicketMachine.java
    Ajustados os issues #1, #2 e #3

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -22,7 +22,7 @@ public class TicketMachine {
     public void inserir(int quantia) throws PapelMoedaInvalidaException {
         boolean achou = false;
         for (int i = 0; i < papelMoeda.length && !achou; i++) {
-            if (papelMoeda[1] == quantia) {
+            if (papelMoeda[i] == quantia) {
                 achou = true;
             }
         }
@@ -37,12 +37,15 @@ public class TicketMachine {
     }
 
     public Iterator<Integer> getTroco() {
-        return null;
+        return saldo;
     }
 
     public String imprimir() throws SaldoInsuficienteException {
         if (saldo < valor) {
             throw new SaldoInsuficienteException();
+        }
+        else {
+            saldo -= valor;
         }
         String result = "*****************\n";
         result += "*** R$ " + saldo + ",00 ****\n";
