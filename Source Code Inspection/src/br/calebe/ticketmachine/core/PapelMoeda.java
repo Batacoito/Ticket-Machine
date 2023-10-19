3d733c6dcd784273282b32282f907b49d056132a
Pedro <72952088+Batacoito@users.noreply.github.com>
2023-10-19 18:39:54 -0300

    Update PapelMoeda.java
    :D

--- Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
@@ -8,7 +8,6 @@ public class PapelMoeda {
 
     protected int valor;
     protected int quantidade;
-    public int a = 0;
 
     public PapelMoeda(int valor, int quantidade) {
         this.valor = valor;
