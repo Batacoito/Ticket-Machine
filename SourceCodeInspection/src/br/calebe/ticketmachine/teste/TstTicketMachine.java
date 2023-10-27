a264a28a97a3873c03afae0b189508c2e95255ba
Pedro <72952088+Batacoito@users.noreply.github.com>
2023-10-26 22:13:17 -0300

    Update TstTicketMachine.java

--- SourceCodeInspection/src/br/calebe/ticketmachine/teste/TstTicketMachine.java
+++ SourceCodeInspection/src/br/calebe/ticketmachine/teste/TstTicketMachine.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import org.junit.Before;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
+import org.junit.Assert.assertThrows;
 
 public class TstTicketMachine {
 
