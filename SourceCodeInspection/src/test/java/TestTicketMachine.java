74f40e863e2a6872a6ea1a379cd7aab0124b6a6e
Pedro <72952088+Batacoito@users.noreply.github.com>
2023-10-27 11:02:16 -0300

    Update TestTicketMachine.java

--- SourceCodeInspection/src/test/java/TestTicketMachine.java
+++ SourceCodeInspection/src/test/java/TestTicketMachine.java
@@ -1,4 +1,4 @@
-package br.calebe.ticketmachine.Tantofaz.core;
+/*package br.calebe.ticketmachine.Tantofaz.core;
 //import Source Code Inspection.src.br.calebe.ticketmachine.core;
 import br.calebe.ticketmachine.Tantofaz.exception.PapelMoedaInvalidaException;
 import br.calebe.ticketmachine.Tantofaz.exception.SaldoInsuficienteException;
@@ -45,5 +45,56 @@ public class TestTicketMachine {
         
         assertEquals(resposta, ticket);
         assertEquals(0, ticketMachine.getSaldo());
+    }
+}
+*/
+package br.calebe.ticketmachine.core;
+
+import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
+import br.calebe.ticketmachine.exception.SaldoInsuficienteException;
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class TestTicketMachine {
+
+    private TicketMachine ticketMachine;
+
+    @Before
+    public void setUp() {
+        ticketMachine = new TicketMachine(50);  
+    }
+
+    @Test
+    public void testInserirValidQuantia() throws PapelMoedaInvalidaException {
+        ticketMachine.inserir(10);  
+        assertEquals(10, ticketMachine.getSaldo());
+    }
+
+    @Test(expected = PapelMoedaInvalidaException.class)
+    public void testInserirInvalidQuantia() throws PapelMoedaInvalidaException {
+        ticketMachine.inserir(7);  
+    }
+
+    @Test
+    public void testImprimirTicketWithSufficientBalance() throws SaldoInsuficienteException,PapelMoedaInvalidaException {
+        ticketMachine.inserir(50);  
+        String ticket = ticketMachine.imprimir();
+        String resposta = "*****************\n";
+        resposta += "*** R$ 50,00 ****\n";
+        resposta += "*****************\n";
+        assertEquals(resposta, ticket);
+        assertEquals(0, ticketMachine.getSaldo());
+    }
+
+    @Test
+    public void testImprimirTicketWithInsufficientBalance() {
+        TicketMachine ticketMachine = new TicketMachine(50); 
+        
+        assertThrows(SaldoInsuficienteException.class, () -> {
+            ticketMachine.imprimir();
+        });
     }
 }
