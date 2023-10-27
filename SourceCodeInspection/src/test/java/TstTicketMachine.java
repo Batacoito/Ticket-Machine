5ff35da7ab7ef3f341d20b92227ee6d6feb90f43
Pedro <72952088+Batacoito@users.noreply.github.com>
2023-10-27 10:46:08 -0300

    Update TstTicketMachine.java

--- SourceCodeInspection/src/test/java/TstTicketMachine.java
+++ SourceCodeInspection/src/test/java/TstTicketMachine.java
@@ -1,7 +1,7 @@
 package br.calebe.ticketmachine.Tantofaz.core;
 //import Source Code Inspection.src.br.calebe.ticketmachine.core;
-import br.calebe.ticketmachine.Tantofaz.exception.SaldoInsuficienteException;
 import br.calebe.ticketmachine.Tantofaz.exception.PapelMoedaInvalidaException;
+import br.calebe.ticketmachine.Tantofaz.exception.SaldoInsuficienteException;
 import org.junit.Test;
 import org.junit.Before;
 
